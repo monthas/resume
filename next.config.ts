105c6be7c08068fa72f37ae5eef68bef66f08385
Monsiree Thassanakathitum <monsiree.t@gmail.com>
2026-01-28 14:52:34 +0700

    Fix build by ci

--- next.config.ts
+++ next.config.ts
@@ -4,6 +4,10 @@ const isProd = process.env.NODE_ENV === 'production';
 const repoName = 'resume';
 
 const nextConfig: NextConfig = {
+  output: 'export',
+  images: {
+    unoptimized: true,
+  },
   basePath: isProd ? `/${repoName}` : ''
 };
 
